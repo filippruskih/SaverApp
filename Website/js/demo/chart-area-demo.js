a5cf904563cd619ff2763ef644d9dbb1bdd7b2cb
Filipp Ruskih <filippruskih@filipps-mbp.wireless.l3.nuigalway.ie>
2023-01-25 12:15:27 +0000

    1.0.1 - Updating latest website details

--- Website/js/demo/chart-area-demo.js
+++ Website/js/demo/chart-area-demo.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
-      label: "Earnings",
+      label: "usage",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [423.6, 413.8, 377.6, 386.9, 298.05, 255.7, 208, 263.1, 347.9, 363.3, 401, 456.98],
     }],
   },
   options: {
@@ -76,9 +76,8 @@ var myLineChart = new Chart(ctx, {
         ticks: {
           maxTicksLimit: 5,
           padding: 10,
-          // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return number_format(value) + 'kWh';
           }
         },
         gridLines: {
@@ -110,7 +109,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ' ' + number_format(tooltipItem.yLabel) + 'kWh';
         }
       }
     }
