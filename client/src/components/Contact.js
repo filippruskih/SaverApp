6e8e88ad9511a6786778f291e6005501f25d704f
Filipp Ruskih <filippruskih@Filipps-MBP.station>
2023-04-01 12:44:16 +0100

    1.3.1 - implementing contact us form via emailjs-com

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../App.css";
+import emailjs from 'emailjs-com';
 import Section from './Section';
 
-const contact = () => {
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.send('service_88pdqqe', 'template_sf5hipn', {
+      from_name: name,
+      from_email: email,
+      message: message,
+    }, 'zA5ES10eDHwL5yleO')
+      .then(() => {
+        alert('Message sent successfully!');
+      }, (error) => {
+        alert('Failed to send the message. ' + error.text);
+      });
+  };
+
   return (
     <Section id='contact'>
       <div className='container pt-2 pb-5'>
@@ -16,38 +36,44 @@ const contact = () => {
         </div>
         <div className='section-content'>
           <div className='row'>
-            <div className='contactcontainer'>
-              <form>
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    className='contactinput'
-                    aria-describedby='emailHelp'
-                    placeholder='Enter Name...'
-                  />
-                </div>
-                <div className='form-group'>
-                  <input
-                    type='email'
-                    className='contactinput'
-                    aria-describedby='emailHelp'
-                    placeholder='Enter email...'
-                  />
-                </div>
-                <div className='form-group'>
-                  <textarea
-                    className='contactinput'
-                    rows='3'
-                    placeholder='Enter Message...'
-                  />
-                </div>
-                <div className='form-group text-center'>
-                  <button className='btn btn-block btn-primary rounded-3 mr-auto ml-auto'>
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
+          <div className='contactcontainer'>
+            <form onSubmit={handleSubmit}>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='contactinput'
+                  aria-describedby='emailHelp'
+                  placeholder='Enter Name...'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='email'
+                  className='contactinput'
+                  aria-describedby='emailHelp'
+                  placeholder='Enter email...'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <textarea
+                  className='contactinput'
+                  rows='3'
+                  placeholder='Enter Message...'
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
+              </div>
+              <div className='form-group text-center'>
+                <button className='btn btn-block btn-primary rounded-3 mr-auto ml-auto'>
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
           </div>
         </div>
       </div>
@@ -55,4 +81,4 @@ const contact = () => {
   );
 };
 
-export default contact;
+export default Contact;
